1e8bdf4b1dea4743d0be734d70e56935834497d9
mtkuwav <math.chaulab@gmail.com>
2026-01-26 23:18:05 +0100

    WIP feat(types): add structs and enums and updated some

--- src/parser/types.rs
+++ src/parser/types.rs
@@ -1,17 +1,21 @@
+use std::collections::HashMap;
+
 #[derive(Debug, PartialEq)]
 struct CueSheet {
     metadata: Metadata,
-    file: Option<String>,
-    tracks: Vec<Track>,
+    files: Vec<FileEntry>, // a cue file can reference more than one files
+    cd_text_file: Option<String>,
     catalog: Option<String>,
 }
 
 #[derive(Debug, PartialEq)]
 struct Track {
     number: u8, // must be between 1 and 99
-    pregap: Option<Index>,
-    index: TrackIndex,
-    flags: Option<Flags>,
+    track_type: TrackType,
+    pregap: Option<Index>, // TODO: implement INDEX 00
+    index_01: Index,
+    additional_indexes: Option<Vec<(u8, Index)>>, // indexes from 02 to more but it's pretty rare
+    flags: Option<Vec<Flags>>,
     metadata: Metadata,
     postgap: Option<Index>,
 }
@@ -26,25 +30,28 @@ struct TrackIndex {
 struct Index {
     minute: u8,
     second: u8,
-    frame: u8, // there are 75 frames per second
+    frame: u8, // there are 75 frames per second, so 1 frame is 1/75 of a second
 }
 
 #[derive(Debug, PartialEq)]
-/// Fields as told by from https://wyday.com/cuesharp/specification.php.
-///
-/// - `ARRANGER`: Name(s) of the arranger(s).
-/// - `COMPOSER`: Name(s) of the composer(s).
-/// - `DISC_ID`: Disc Identification information.
-/// - `GENRE`: Genre Identification and Genre information.
-/// - `ISRC`: ISRC Code of each track.
-/// - `MESSAGE`: Message from the content provider and/or artist.
-/// - `PERFORMER`: Name(s) of the performer(s).
-/// - `SONGWRITER`: Name(s) of the songwriter(s).
-/// - `TITLE`: Title of album name or Track Titles.
-/// - `TOC_INFO`: Table of Content information.
-/// - `TOC_INFO2`: Second Table of Content information.
-/// - `UPC_EAN`: UPC/EAN code of the album.
-/// - `SIZE_INFO`: Size information of the Block.
+struct FileEntry {
+    file_name: String,
+    file_type: FileType,
+    tracks: Vec<Track>,
+}
+
+#[derive(Debug, PartialEq)]
+/// Other fields and commands as specified by https://www.gnu.org/software/ccd2cue/manual/html_node/CUE-sheet-format.html,
+/// - `arranger`: Name(s) of the arranger(s).
+/// - `composer`: Name(s) of the composer(s).
+/// - `disc_id`: Disc Identification information.
+/// - `genre`: Genre Identification and Genre information.
+/// - `isrc`: ISRC Code of each track.
+/// - `message`: Message from the content provider and/or artist.
+/// - `performer`: Name(s) of the performer(s).
+/// - `songwriter`: Name(s) of the songwriter(s).
+/// - `title`: Title of album name or Track Titles.
+/// - `other_rem`: Additional REM comments, such as `DATE` for example.
 struct Metadata {
     /// Name(s) of the arranger(s).
     arranger: Option<String>,
@@ -64,26 +71,61 @@ struct Metadata {
     songwriter: Option<String>,
     /// Title of album name or Track Titles.
     title: Option<String>,
-    /// Table of Content information.
-    toc_info: Option<String>,
-    /// Second Table of Content information.
-    toc_info2: Option<String>,
-    /// UPC/EAN code of the album.
-    upc_ean: Option<String>,
-    /// Size information of the Block.
-    size_info: Option<String>,
+    /// Additional REM comments, such as `DATE` for example.
+    other_rem: Option<HashMap<String, String>>,
 }
 
 #[derive(Debug, PartialEq)]
 /// Track special sub-code flags, rarely used today.
 ///
-/// - `PRE`: **PRE**-emphasis enabled (audio tracks only)
-/// - `DCP`: **D**igital **C**opy **P**ermitted
-/// - `FourCH` *(4CH)*: **4** **CH**annel audio
-/// - `SCMS`: **S**erial **C**opy **M**anagement **S**ystem
+/// | Value | Description |
+/// |------|-------|
+/// | `PRE` | **PRE**-emphasis enabled (audio tracks only) |
+/// | `DCP` | **D**igital **C**opy **P**ermitted |
+/// | `FourCH` *(4CH)* | **4** **CH**annel audio |
+/// | `SCMS` | **S**erial **C**opy **M**anagement **S**ystem |
 enum Flags {
     PRE,
     DCP,
     FourCH,
-    SCMS
+    SCMS,
+}
+
+#[derive(Debug, PartialEq)]
+/// Track data modes, as defined in [ccd2cue manual](https://www.gnu.org/software/ccd2cue/manual/html_node/MODE-_0028Compact-Disc-fields_0029.html#MODE-_0028Compact-Disc-fields_0029).
+///
+/// *Note: The modes marked with ‘\*’ are not defined in the original CUE sheet format specification.*
+///
+/// | Value | Description | Sector Size |
+/// |------|-------|-----|
+/// | `AUDIO` | Audio/Music (588 samples) | 2352 |
+/// | `CDG` | Karaoke CD+G | 2448 |
+/// | `MODE1/2048` | CD-ROM Mode 1 Data (cooked) | 2048 |
+/// | `MODE1/2352` | CD-ROM Mode 1 Data (raw) | 2352 |
+/// | `MODE2/2048` | CD-ROM XA Mode 2 Data (form 1) * | 2048 |
+/// | `MODE2/2324` | CD-ROM XA Mode 2 Data (form 2) * | 2324 |
+/// | `MODE2/2336` | CD-ROM XA Mode 2 Data (form mix) | 2336 |
+/// | `MODE2/2352` | CD-ROM XA Mode 2 Data (raw) | 2352 |
+/// | `CDI/2336` | CD-i Mode 2 Data | 2336 |
+/// | `CDI/2352` | CD-i Mode 2 Data (raw) | 2352 |
+enum TrackType {
+    Audio,
+    CDG,
+    Mode1_2048,
+    Mode1_2352,
+    Mode2_2048,
+    Mode2_2324,
+    Mode2_2336,
+    Mode2_2352,
+    CDI_2336,
+    CDI_2352,
+}
+
+#[derive(Debug, PartialEq)]
+enum FileType {
+    Binary,
+    Motorola,
+    Aiff,
+    Wave,
+    Mp3,
 }
